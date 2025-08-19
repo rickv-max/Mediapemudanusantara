1178c6d3708ef54e7a9347a71fdf6ad7868bebb6
rickv-max <rickymaulana080503@gmail.com>
2025-08-19 21:29:23 +0700

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
   return (
     <main className="py-10">
       <h1 className="text-2xl md:text-3xl font-bold text-teal-700 text-center">Tentang Kami</h1>
-      <p className="max-w-3xl mx-auto px-4 text-center text-gray-700 mt-4">
+      <p className="max-full mx-auto px-0 text-center text-gray-700 mt-4">
         Media Pemuda Nusantara adalah komunitas kreatif yang bergerak di bidang
         live streaming & dokumentasi. Kami berkomitmen menghadirkan layanan
         terbaik dengan tim profesional.
